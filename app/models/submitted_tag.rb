4d5e1f03b400fef79fc83e64ef7f7aa36ff6e96f
RasPhilCo <Ras@PhilCo>
2014-08-23 16:39:50 -0400

    Update check for match algorithm

--- app/models/submitted_tag.rb
+++ app/models/submitted_tag.rb
@@ -15,7 +15,7 @@ class SubmittedTag < ActiveRecord::Base
     return true if check_submitted_tag
   end
 
-  def check_submitted_tag    
+  def old_depricated_method_for reference    
     #Affine Transformation
     from = Tag.find(1).dots_arr
     to = @dots_arr
@@ -33,6 +33,46 @@ class SubmittedTag < ActiveRecord::Base
     if err.between?(-1, 260) #Tolerance range
       return true
     else
+      return false
+    end
+  end
+
+
+
+  # Needs to be cleaned up (not very DRY) but it's working with a 0.05% tolerance!
+  def check_submitted_tag
+    tolerance = 0.05
+    dists = {}
+    dots = Tag.find(1).dots_arr
+    handshakes = [[0,1],[0,2],[0,3],[0,4],[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+      handshakes.each do |hand|
+        dists[hand] = Math.hypot(dots[hand[1]][0]-dots[hand[0]][0],dots[hand[1]][1]-dots[hand[0]][1])
+      end
+
+    dists_subs = {}
+    subs = @dots_arr
+      handshakes.each do |hand|
+        dists_subs[hand] = Math.hypot(subs[hand[1]][0]-subs[hand[0]][0],subs[hand[1]][1]-subs[hand[0]][1])
+      end
+
+    match_truth = {}
+    dists_subs.values.each do |dist_sub|
+
+      dists.values.each do |dist|
+        case dist_sub
+        when ((dist*(1 - tolerance))..(dist*(1 + tolerance)))
+          match_truth[dist_sub]= true
+          # puts true
+        else
+          puts false
+        end
+      end
+
+    end
+
+    if match_truth.count == 10
+      return true
+    else 
       return false
     end
   end
